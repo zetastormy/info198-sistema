57d1646af00a5c14711a5c929cb4a2cdbb53f97d
DsAlenRupailaf <alenrupailaft@gmail.com>
2025-11-28 14:29:58 -0300

    Añadi espacio de trabajo para la search engine

---
+++ src/search_engine/search_engine.cpp
@@ -0,0 +1,13 @@
+#include "../include/dotenv.h"
+#include <cstring>
+#include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+using namespace std;
+
+int main (int argc, char**argv) {
+    return 0;
+}
+
+int startServerSocket(int port) {
+}
